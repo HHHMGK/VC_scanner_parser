10832fef95fca84b549f015b80c42fdae843ce01
HUY <ngoduchuy71123@gmail.com>
2024-04-13 14:30:53 +0700

    NEW (better) tokenizer

--- main.py
+++ main.py
@@ -5,25 +5,35 @@ from pathlib import Path
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Scanner phase of Compiling a file')
-    parser.add_argument('--folder', type=str, default="./examples/", help='Path to the folder of the file')
+    parser.add_argument('--input_folder', type=str, default="./examples/", help='Path to the folder of the file')
+    parser.add_argument('--output_folder', type=str, default="./examples/", help='Path to the folder of the output file')
     parser.add_argument('--filename', type=str, default="example_gcd", help='Name of the file')
     args = parser.parse_args()
 
     # Path to the source code file
-    path = Path(args.folder) / f"{args.filename}.vc"
+    inpath = Path(args.input_folder) / f"{args.filename}.vc"
     # Read data from source code file
     data = ""
-    with open(path, 'r') as file:
+    with open(inpath, 'r') as file:
         data = file.read()
     
-    tokens = tokenizer.tokenizer(data)
-    with open('./examples/example_gcd.vctok', 'w+') as file:
-    # with open('./examples/example_fib.vctok', 'w+') as file:
-        file.write('\n'.join(tokens))
+    tokens = tokenizer.tokenizer2(data)
+    tokpath = Path(args.output_folder) / f"{args.filename}.vctok"
+    with open(tokpath, 'w+') as file:
+        for tok, _ in tokens:
+            file.write(f"{tok}\n")
 
-    verbose_tokens = scanner.scan_tokens(data)
-    with open('./examples/example_gcd.verbose.vctok', 'w+') as file:
-        for tok, state, kind in verbose_tokens:
-            file.write(f"Spelling = \"{tok}\", kind = {state} [{kind}], position = ")
+    # verbose_tokens = scanner.scan_tokens(data)
+    verbose_tokens = tokens
+    verbosepath = Path(args.output_folder) / f"{args.filename}.verbose.vctok"
+    with open(verbosepath, 'w+') as file:
+        # for tok, state, kind in verbose_tokens:
+        #     tok, (line, col) = tok
+        #     file.write(f"Spelling = \"{tok}\", kind = {state} [{kind}], position = {line}({col})..{line}({col+len(tok)-1})")
+        for tok in verbose_tokens:
+            tok, (line, col) = tok
+            state = 0
+            kind = "*"
+            file.write(f"Spelling = \"{tok}\", kind = {state} [{kind}], position = {line}({col})..{line}({col+len(tok)-1})\n")
     
     
--- scanner/tokenizer.py
+++ scanner/tokenizer.py
@@ -16,6 +16,8 @@ with open(DATAJSON, 'r') as file:
     comment = data['comment']
     multi_comment = data['multi_comment']
 
+def removeTabs(text : str) -> str:
+    return text.replace('\t','    ')
 
 def tokenizer(text : str):
     lines = text.split('\n')
@@ -33,14 +35,12 @@ def tokenizer(text : str):
 
         if multi_comment[0] in line:
             i = line.find(multi_comment[0])
-            print(line,"@ ", i, line[:i])
             if i != 0:
                 tokens.extend(line[:i].split())
             in_comment = True
 
         if multi_comment[1] in line:
             i = line.find(multi_comment[1])+1
-            print(line,"# ", i, line[i+1:])
             if i < len(line)-1:
                 tokens.extend(line[i+1:].split())
             in_comment = False
@@ -91,11 +91,95 @@ def tokenizer(text : str):
     # Fix for double operators like '==', '!=', '<=', '>=', '&&', '||'
     for i in range(len(refined_tokens)-1):
         if refined_tokens[i]!='' and refined_tokens[i+1]!='' and refined_tokens[i]+refined_tokens[i+1] in operator:
-            print(i,i+1,refined_tokens[i], refined_tokens[i+1])
             refined_tokens[i] = refined_tokens[i]+refined_tokens[i+1]
             refined_tokens[i+1] = ''
 
     refined_tokens = [tok for tok in refined_tokens if tok != '']
 
     refined_tokens.append('$')
+    
     return refined_tokens
+
+def tokenizer2(text: str) -> list:
+    # Returns the list of (token,positional: start line,pos) pairs 
+    
+    lines = removeTabs(text).split('\n')
+    tokens = []
+    cnt_line = 0
+    in_multi_comment = False
+    for line in lines:
+        line+= ' '
+        in_comment = in_multi_comment
+        in_string = False
+        token = ''
+        i = 0
+        while i < len(line):
+            if in_comment:
+                # Stop multi comment
+                if i+1 < len(line) and line[i]+line[i+1] == multi_comment[1]:
+                    in_comment = False
+                    in_multi_comment = False
+                    i += 2
+                    continue        
+                i += 1
+                continue
+            
+            # String
+            if not in_string and line[i] == '\"':
+                if token != '':
+                    tokens.append((token,(cnt_line,i-len(token))))
+                    token = ''
+                in_string = True
+                token += line[i]
+                i += 1
+                continue
+            if in_string:
+                token += line[i]
+                if line[i] == '\"':
+                    in_string = False
+                i += 1
+                continue
+            
+            # Start multi comment
+            if i+1 < len(line) and line[i]+line[i+1] == multi_comment[0]:
+                in_comment = True
+                in_multi_comment = True
+                i+=2
+                continue
+
+            # Skip when line comment
+            if i+1 < len(line) and line[i]+line[i+1] == comment:
+                break
+            
+
+            c = line[i]
+            c2 = ''
+            if i+1 < len(line):
+                c2 = line[i+1]
+            
+            if c2 != '' and c+c2 in operator:
+                if token != '':
+                    tokens.append((token,(cnt_line,i-len(token))))
+                    token = ''
+                tokens.append((c+c2,(cnt_line,i)))
+                i += 2
+                continue
+
+            if c in seperator + operator or c==' ':
+                if token != '':
+                    tokens.append((token,(cnt_line,i-len(token))))
+                    token = ''
+                if c != ' ':
+                    tokens.append((c,(cnt_line,i)))
+                i += 1
+                continue
+
+            
+            token += c
+            i+=1
+
+        cnt_line += 1
+
+    tokens.append(('$', (cnt_line,0)))
+    return tokens   
+
